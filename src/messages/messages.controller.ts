cba4f3a3832fcdff33e3e568a6bdb44f0e93f8ef
Rolani Maluleka <Rolani.Maluleka@absa.africa>
2024-02-29 13:36:40 +0200

    adding Dependency Injections'DI' inside of my  application

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,38 @@
-import { Controller, Get, Post, Body, Param} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, NotFoundException, BadRequestException} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service'; 
 
 @Controller('messages')
 export class MessagesController {
+    constructor(public messagesService: MessagesService){
+    }
+
     @Get()
     listMessages(){
+        return this.messagesService.findAll();
     }
 
     @Post()
-    createMessage(@Body() body: CreateMessageDto){
-        console.log(body);
+    async createMessage(@Body() body: CreateMessageDto){
+       const createdMessage = this.messagesService.create(body.content);
+
+       if(!createdMessage){
+        throw new BadRequestException("Message is of type string not Numbers")
+       }
+
+       return createdMessage
     }
 
     @Get('/:id')
-    getMessages(@Param('id') id: string){
-        console.log(id);
+    async getMessages(@Param('id') id: string){
+        
+        const message = await this.messagesService.findOne(id);
+
+        if(!message){
+            throw new NotFoundException('Message with that id does not exist') ;
+        }
+
+        return message
     }
 
 }
